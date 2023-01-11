24ede883ce9b31b64efc067b1f922b5d56d08e23
mrog <mrog@xtain.net>
2023-01-11 11:35:44 +0100

    push var and vendor

---
+++ var/cache/dev/twig/53/532a1ae16a19f1c24796f7548fba6777.php
@@ -0,0 +1,200 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* base.html.twig */
+class __TwigTemplate_470575d1c7205fea7a9d4002753cf857 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
+        ";
+        // line 8
+        echo "        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400\">    <!-- Google web font \"Open Sans\" -->
+        <link rel=\"stylesheet\" href=\"./css/bootstrap.min.css\">                                        <!-- https://getbootstrap.com/ -->
+        <link rel=\"stylesheet\" href=\"./css/templatemo-style.css\">                                     <!-- Templatemo style -->
+
+
+        <script>
+            var renderPage = true;
+
+            if(navigator.userAgent.indexOf('MSIE')!==-1
+                || navigator.appVersion.indexOf('Trident/') > 0){
+                /* Microsoft Internet Explorer detected in. */
+                alert(\"Please view this in a modern browser such as Chrome or Microsoft Edge.\");
+                renderPage = false;
+            }
+        </script>
+
+    </head>
+    <body>
+        ";
+        // line 26
+        $this->displayBlock('body', $context, $blocks);
+        // line 27
+        echo "        <!-- load JS -->
+        <script src=\"./js/jquery-3.2.1.slim.min.js\"></script>         <!-- https://jquery.com/ -->
+        <script>
+
+            /* DOM is ready
+            ------------------------------------------------*/
+            \$(function(){
+
+                if(renderPage) {
+                    \$('body').addClass('loaded');
+                }
+
+                \$('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
+            });
+
+        </script>
+    </body>
+</html>
+";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Welcome!";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 26
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "base.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  125 => 26,  106 => 5,  78 => 27,  76 => 26,  56 => 8,  51 => 5,  45 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>{% block title %}Welcome!{% endblock %}</title>
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
+        {# Run `composer require symfony/webpack-encore-bundle` to start using Symfony UX #}
+        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400\">    <!-- Google web font \"Open Sans\" -->
+        <link rel=\"stylesheet\" href=\"./css/bootstrap.min.css\">                                        <!-- https://getbootstrap.com/ -->
+        <link rel=\"stylesheet\" href=\"./css/templatemo-style.css\">                                     <!-- Templatemo style -->
+
+
+        <script>
+            var renderPage = true;
+
+            if(navigator.userAgent.indexOf('MSIE')!==-1
+                || navigator.appVersion.indexOf('Trident/') > 0){
+                /* Microsoft Internet Explorer detected in. */
+                alert(\"Please view this in a modern browser such as Chrome or Microsoft Edge.\");
+                renderPage = false;
+            }
+        </script>
+
+    </head>
+    <body>
+        {% block body %}{% endblock %}
+        <!-- load JS -->
+        <script src=\"./js/jquery-3.2.1.slim.min.js\"></script>         <!-- https://jquery.com/ -->
+        <script>
+
+            /* DOM is ready
+            ------------------------------------------------*/
+            \$(function(){
+
+                if(renderPage) {
+                    \$('body').addClass('loaded');
+                }
+
+                \$('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
+            });
+
+        </script>
+    </body>
+</html>
+", "base.html.twig", "/Users/mrog/Projekte/louisa/templates/base.html.twig");
+    }
+}
